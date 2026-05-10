cb65bbce39cbb40ad92a88727b296b6a6766a384
Ahmed-Sadek89 <ahmeamohamed161@gmail.com>
2026-05-10 23:07:20 +0300

    start again !

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,16 +1,11 @@
-import Navbar from '@/components/public-layout/navbar'
-import Topnav from '@/components/public-layout/top-nav'
 import React from 'react'
 
 const layout = ({ children }: { children: React.ReactNode }) => {
     return (
         <>
-            <Topnav />
-            <Navbar />
             <main className='min-h-screen'>
                 {children}
             </main>
-            footer
         </>
     )
 }
