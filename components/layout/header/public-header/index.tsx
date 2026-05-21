99367800bf18cce02d31e6eb70c89a8134f6af3e
Ahmed-Sadek89 <ahmeamohamed161@gmail.com>
2026-05-21 10:55:52 +0300

    fix: remove third section

--- components/layout/header/public-header/index.tsx
+++ components/layout/header/public-header/index.tsx
@@ -1,13 +1,11 @@
 import Topnav from './components/top-nav'
 import FirstNav from './components/first-nav'
-import ThirdInfo from './components/third-info'
 
 const PublicHeader = () => {
     return (
         <header>
             <Topnav />
             <FirstNav />
-            <ThirdInfo />
         </header>
     )
 }
