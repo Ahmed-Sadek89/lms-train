eeba32c174d57a755625d2fe6d70e9793a033c49
Ahmed-Sadek89 <ahmeamohamed161@gmail.com>
2026-06-20 15:52:37 +0300

    feat: pages for courses

---
+++ app/(public)/courses/[slug]/gift/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CourseDetailsGiftPage = () => {
+  return (
+    <div>CourseDetailsGiftPage</div>
+  )
+}
+
+export default CourseDetailsGiftPage
---
+++ app/(public)/courses/[slug]/watch/[series]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CourseDetailsWatchSeriesPage = () => {
+  return (
+    <div>CourseDetailsWatchSeriesPage</div>
+  )
+}
+
+export default CourseDetailsWatchSeriesPage
